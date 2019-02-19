5264bd4f7d86b15c0afd241704704cd8153788ad
IDea Factory <dev-team@ideafactory.fr>
2019-02-19 15:51:59 +0100

    Récupération des réservations effectuées par l'utilisateur

--- src/app/shared/services/tournees.service.ts
+++ src/app/shared/services/tournees.service.ts
@@ -1,43 +1,61 @@
 import { TourneeInterface } from './../interfaces/tournee';
 import { Injectable } from '@angular/core';
 import * as moment from 'moment';
+import { ResaService } from './resa.service';
+import { ResaModel } from '../models/resa-model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TourneesService {
+  private resaService: ResaService = new ResaService();
 
-  constructor() { }
+  constructor() {
+   }
 
   public getTournees(): Promise<Array<TourneeInterface>> {
     return new Promise((resolve) => {
       const tournees: Array<TourneeInterface> = new Array<TourneeInterface>();
       const today: moment.Moment = moment();
-
-      tournees.push(
+      this.resaService.getAll().then((resas) => {
+        tournees.push(
           {
             hour: today.clone().hour(8).minute(0).second(0),
-            dispo: 8,
+            dispo: this.setDispos(resas, today.clone().hour(8).minute(0).second(0)),
             resa: 1
           },
           {
             hour: today.clone().hour(11).minute(0).second(0),
-            dispo: 8,
+            dispo: this.setDispos(resas, today.clone().hour(11).minute(0).second(0)),
             resa: 1
           },
           {
             hour: today.clone().hour(14).minute(0).second(0),
-            dispo: 8,
+            dispo: this.setDispos(resas, today.clone().hour(14).minute(0).second(0)),
             resa: 1
           },
           {
             hour: today.clone().hour(17).minute(0).second(0),
-            dispo: 8,
+            dispo: this.setDispos(resas, today.clone().hour(17).minute(0).second(0)),
             resa: 1
           },
         );
 
-      resolve(tournees);
+        resolve(tournees);
+      });
+
     });
+  }
+
+  private setDispos(resas: Array<ResaModel>, tourHour: moment.Moment): number {
+
+    const indice: number = resas.findIndex((obj) => {
+      return obj.getTourDate().isSame(tourHour, 'hour');
+    });
+
+    if (indice !== -1) {
+      return 8 - resas[indice].getPlaces();
+    }
+    return 8;
   }
 }
